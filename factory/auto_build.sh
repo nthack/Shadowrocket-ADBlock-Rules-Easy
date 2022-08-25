3a7e91693a4b7e3e373c3f16e048ff3643cd539c
nthack <nthack5730@gmail.com>
2022-08-25 15:58:46 +0800

    更新auto build

--- factory/auto_build.sh
+++ factory/auto_build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-Path=./factory
-cd $Path
-
 ## 构建一系列的规则列表
 python3 ad.py
 python3 gfwlist.py
 python3 build_confs.py
+
+
+cd ..
+git add .
+git commit -m 'Daily Build' -m '更新GFWList及所有规则'
+git push
