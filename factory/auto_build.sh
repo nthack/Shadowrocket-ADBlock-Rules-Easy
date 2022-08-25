07a180c8cec1b338c3f11b1fbfa45f975e0fb780
nthack <nthack5730@gmail.com>
2022-08-25 08:29:12 +0800

    update auto_build.sh, 添加执行权限，修正内容，用于Github Action

--- factory/auto_build.sh
+++ factory/auto_build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
+Path=./factory
+cd $Path
+
 ## 构建一系列的规则列表
-# python3.9 ad.py
-# python3.9 gfwlist.py
-python3.9 build_confs.py
-cd ..
-
-
-## 自动推送
-# git add .
-# git commit -m "Daily build" -m "已合并最新的去广告规则及 GFWList"
-# git push
+python3 ad.py
+python3 gfwlist.py
+python3 build_confs.py
